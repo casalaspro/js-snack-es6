084d2cea7dd3c126189c50a359e2d4bd82d9e7bf
casalaspro <casalaspro.alessandro@gmail.com>
2024-03-19 15:55:56 +0100

    REFACTORY - deleted students comment

--- js/app.js
+++ js/app.js
@@ -72,19 +72,6 @@ function createStudent(id, name, grade){
   return student;
 }
 
-/*
-
-Id   Name                    Grades
-213  Marco della Rovere       78
-110  Paola Cortellessa        96
-250  Andrea Mantegna 	        48
-145  Gaia Borromini           74
-196  Luigi Grimaldello 	      68
-102  Piero della Francesca    50
-120  Francesca da Polenta     84
-
-*/
-
 studentsChart.push(createStudent(213, "Marco della Rovere", 78));
 studentsChart.push(createStudent(110, "Paola Cortellessa", 96));
 studentsChart.push(createStudent(250, "Andrea Mantegna", 48));
