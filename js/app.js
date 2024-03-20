6d6d18d4fec6e810b1a015611a08be3cff9528fa
casalaspro <casalaspro.alessandro@gmail.com>
2024-03-20 14:27:38 +0100

    ADD - snack 3 array

--- js/app.js
+++ js/app.js
@@ -130,5 +130,44 @@ console.log(capitalizedNames);
 console.log(gradeSelection);
 console.log(gradeIdSelection);
 
+/*
+
+SNACK 3
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare in console la bici con peso minore utilizzando destructuring e template literal
+SNACK 4
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+BONUS
+Stampare in pagina oltre che in console!
+
+*/
+
+const bikes = [
+  {
+    name: "Bianchi Specialissima",
+    weight: 6.7
+  },
+  {
+    name: "Canyon Ultimate CF",
+    weight: 6.8
+  },
+  {
+    name: "Rose X-Lite Six Disc",
+    weight: 6.9
+  },
+  {
+    name: "Sarto asola",
+    weight: 6.7
+  },
+  {
+    name: "Time Alpe D’Huez 01",
+    weight: 6.2
+  },
+];
 
 
